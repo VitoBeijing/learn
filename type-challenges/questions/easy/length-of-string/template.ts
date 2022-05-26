593d788b1830fa83a3266cbc90ece7b37faf7816
VitoBeijing <hanxiao_do@126.com>
2022-05-27 00:15:57 +0800

    类型体操-length-of-string

---
+++ type-challenges/questions/easy/length-of-string/template.ts
@@ -0,0 +1,4 @@
+type StringToArray<S extends string> = S extends `${infer T}${infer R}`
+    ? [T, ...StringToArray<R>]
+    : [];
+type LengthOfString<S extends string> = StringToArray<S>['length'];
