e898aa8d88eb2182ff260690f1775d771e35e1fc
VitoBeijing <hanxiao_do@126.com>
2022-06-08 00:16:40 +0800

    fix test-case

--- type-challenges/questions/medium/is-union/test-case.ts
+++ type-challenges/questions/medium/is-union/test-case.ts
@@ -1,11 +1,16 @@
 import type {Equal, Expect} from '@type-challenges/utils';
 
 type cases = [
-    Expect<Equal<IsNever<never>, true>>,
-    Expect<Equal<IsNever<never | string>, false>>,
-    Expect<Equal<IsNever<''>, false>>,
-    Expect<Equal<IsNever<undefined>, false>>,
-    Expect<Equal<IsNever<null>, false>>,
-    Expect<Equal<IsNever<[]>, false>>,
-    Expect<Equal<IsNever<{}>, false>>
+    Expect<Equal<IsUnion<string>, false>>,
+    Expect<Equal<IsUnion<string | number>, true>>,
+    Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+    Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+    Expect<Equal<IsUnion<{a: string} | {a: number}>, true>>,
+    Expect<Equal<IsUnion<{a: string | number}>, false>>,
+    Expect<Equal<IsUnion<[string | number]>, false>>,
+    // Cases where T resolves to a non-union type.
+    Expect<Equal<IsUnion<string | never>, false>>,
+    Expect<Equal<IsUnion<string | unknown>, false>>,
+    Expect<Equal<IsUnion<string | any>, false>>,
+    Expect<Equal<IsUnion<string | 'a'>, false>>
 ];
